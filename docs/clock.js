7f07ccb94d642596ca3d119b54617183060ac747
Uriel Arevalo <urievalo@hotmail.com>
2021-09-02 09:24:03 -0500

    agregando logica para  25-09-2021

--- docs/clock.js
+++ docs/clock.js
@@ -4,6 +4,8 @@ console.log(long1);
 const mark2 = document.getElementById("mark2");
 const long2 = mark2.getTotalLength();
 console.log(long2);
+const cohetes = document.getElementById("cohetes");
+const leyenda = document.getElementById("leyenda");
 
 setInterval(function () {
   function r(el, deg) {
@@ -15,3 +17,14 @@ setInterval(function () {
   r(min, 6 * d.getMinutes());
   r(hour, 30 * (d.getHours() % 12) + d.getMinutes() / 2);
 }, 1000);
+
+let fecha = new Date();
+let mes = fecha.getMonth() + 1;
+let dia = fecha.getDate();
+
+let c = mes === 9 && dia === 2;
+
+if (c) {
+  cohetes.style.opacity = 1;
+  leyenda.style.opacity = 1;
+}
